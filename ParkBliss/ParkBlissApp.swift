96c465c73be20a1e62dcebdd050c769d83f45201
baobaollf <55776365+baobaollf@users.noreply.github.com>
2022-03-21 01:39:03 -0500

    Initial Commit

---
+++ ParkBliss/ParkBlissApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkBlissApp.swift
+//  ParkBliss
+//
+//  Created by baobaollf on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ParkBlissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
