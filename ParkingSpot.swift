cdf848418ab1ce3613cd54f02712c1efeb119cd2
baobaollf <55776365+baobaollf@users.noreply.github.com>
2022-04-19 12:30:00 -0500

    apply button style

--- ParkingSpot.swift
+++ ParkingSpot.swift
@@ -36,6 +36,7 @@ struct ParkingSpot: View {
                     Text("Share").padding()
                     
                 }
+                .buttonStyle(GrowingButton())
                 
                 
             }
